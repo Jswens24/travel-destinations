0a7e087f518adeb3691749d3d9ef2f4af4b8db31
Jessica Swenson <jessicamswens@gmail.com>
2023-02-28 07:34:11 -0700

    added routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <header>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='addDestination'>Add New Destination!</NavLink>
+        </header>
+    )
+}
+
+export default Header
